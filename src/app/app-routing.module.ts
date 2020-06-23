f26c5332c1ba4601b3b339d91378a53fbd8a7f88
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-23 09:46:37 +0700

    refactor code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageComponent } from './page/homepage/homepage.component';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,11 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './page/login/login.component';
 import { SharedModule } from './shared/shared.module';
-import { LoginRoutingModule } from './login/login-routing.module';
+import { LoginRoutingModule } from './page/login/login-routing.module';
 import { NzMessageService } from 'ng-zorro-antd';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageComponent } from './page/homepage/homepage.component';
 
 registerLocaleData(en);
 

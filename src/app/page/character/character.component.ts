3e966a571993e7f89df2dcf0c038a99f0462c308
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-25 09:43:32 +0700

    config character routing

--- src/app/page/character/character.component.ts
+++ src/app/page/character/character.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { CharacterService } from 'src/app/service/character.service';
 import { Character } from 'src/app/shared/model/character';
 import { forkJoin } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-character',
@@ -13,7 +14,8 @@ export class CharacterComponent implements OnInit, OnChanges {
   characters: Character[]
   searchText;
   constructor(
-    private characterService: CharacterService
+    private characterService: CharacterService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -32,6 +34,12 @@ export class CharacterComponent implements OnInit, OnChanges {
     else{
     this.characterService.getCharacters().subscribe(characters => this.characters = characters)
     }
+  }
+
+  selectCharacter(data){
+    localStorage.setItem('characterCurrent',data.url);
+    var id = data.url.split("/").slice(-1).pop()
+    this.router.navigate(['character-detail',id])
   }
 
 }

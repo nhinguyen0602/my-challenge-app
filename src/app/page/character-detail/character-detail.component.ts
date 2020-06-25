219e270af1007243c0f6e2c00402a3a806321597
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-25 14:24:09 +0700

    show character detail

--- src/app/page/character-detail/character-detail.component.ts
+++ src/app/page/character-detail/character-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from 'src/app/shared/model/character';
+import { CharacterService } from 'src/app/service/character.service';
 
 @Component({
   selector: 'app-character-detail',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterDetailComponent implements OnInit {
 
-  constructor() { }
+  character: Character
+
+  constructor(
+    private characterService: CharacterService
+  ) { }
 
   ngOnInit(): void {
+    this.getCharacter()
+  }
+
+  getCharacter(){
+    var url = localStorage.getItem('characterCurrent')
+    this.characterService.getCharacter(url).subscribe(charater => this.character = charater)
   }
 
 }
